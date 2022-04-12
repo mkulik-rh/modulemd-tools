9a22bcd28606b0686604355da35098f1a92de696
Marek Kulik <mkulik@redhat.com>
2022-04-12 15:08:39 +0200

    Test PR tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,6 @@ set -ex
 SITEPACKAGES=true tox -vvv -e py310,flake8
 rm -rf .tox/ build/ dist/ modulemd_tools.egg-info/
 find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
+
+
+# Really important change
