6f8b3746b7a9f739c75387ffc2aa4174e56fa518
Marek Kulik <mkulik@redhat.com>
2022-04-12 15:59:34 +0200

    check pr

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ rm -rf .tox/ build/ dist/ modulemd_tools.egg-info/
 find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
 
 
-# Really important change
+# Really important change 2
